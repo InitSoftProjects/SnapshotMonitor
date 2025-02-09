fa4e6bc4030286ef470d802f76d9ddf4ad120fe6
vijeesh v <vijeeshvkd@gmail.com>
2025-02-09 03:04:14 +0000

    reset implementation

--- webapp/controller/CreateSnapshot.controller.js
+++ webapp/controller/CreateSnapshot.controller.js
@@ -9,11 +9,25 @@ function (Controller, JSONModel, formatter, UIComponent) {
     return Controller.extend("com.arumatech.snapshotmonitor.controller.CreateSnapshot", {
         formatter: formatter,
         onInit: function () {
-
+            var oModel = new JSONModel({
+                snapID: "",
+                snapType: "",
+                snapDate: ""
+            });
+            this.getView().setModel(oModel, "viewModel");
         },
 
         onCreateSnapshot: function() {
             this._navBack();
+        },
+
+        onResetPress: function() {
+            var oModel = this.getView().getModel("viewModel");
+            oModel.setData({
+                snapID: "",
+                snapType: "",
+                snapDate: ""
+            });
         },
 
         _navBack: function() {
